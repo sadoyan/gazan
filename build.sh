390abc39d429713bb372e3d8ed978c731a8c346b
Ara Sadoyan <ara.sadoyan@netangels.net>
2022-03-12 20:18:43 +0400

    Build script changes

--- build.sh
+++ build.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 export GOPATH=`pwd`
-
-#go get github.com/go-sql-driver/mysql
-#go get gopkg.in/gomail.v2
-#go get github.com/google/uuid
 go get gopkg.in/ini.v1
 
-go build -o /tmp/GOProxy src/start.go
+go build -o /tmp/Gazan src/start.go
 
