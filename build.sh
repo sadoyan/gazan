51453d37c422a8ba33e9badf5d1f02ad279738d1
Ara Sadoyan <ara.sadoyan@netangels.net>
2022-05-04 13:36:56 +0400

    ho

--- build.sh
+++ build.sh
@@ -5,14 +5,19 @@ export BASICPASS='Te$ting'
 export JWTSECRET='Super$ecter123765@'
 
 
-export GOPATH=`pwd`
-go get gopkg.in/ini.v1
-go get github.com/golang-jwt/jwt
-go build -o /tmp/Gazan src/start.go
+export GOPATH=/opt/GOLang/goext
+#go get gopkg.in/ini.v1
+#go get github.com/golang-jwt/jwt
+#go build -o /tmp/Gazan src/start.go
+go build -o /tmp/Gazan main.go
 
 #reflex -d none -r '.go'  -v  -s -- sh -c  'go run src/start.go -up=config/upstreams.json'
 
-reflex -d none -r '.'  -s -- sh -c  'go run src/start.go -config config.ini -up=config/upstreams.json'
+reflex -d none -r '.'  -s -- sh -c  'go run ./ -config config.ini -up=cfgjson/upstreams.json'
+
+
+
+#reflex -d none -r '.'  -s -- sh -c  'go run src/start.go -config config.ini -up=config/upstreams.json'
 #reflex -d fancy -r '.'  -s -- sh -c  'go run src/start.go -up=config/upstreams.json'
 
 
--- go.mod
+++ go.mod
@@ -7,4 +7,15 @@ require (
 	gopkg.in/ini.v1 v1.66.4
 )
 
-require github.com/stretchr/testify v1.7.1 // indirect
+replace configs => ./configs
+
+replace mainfiles => ./mainfiles
+
+replace utils => ./utils
+
+require (
+	configs v0.0.0-00010101000000-000000000000 // indirect
+	github.com/stretchr/testify v1.7.1 // indirect
+	mainfiles v0.0.0-00010101000000-000000000000 // indirect
+	utils v0.0.0-00010101000000-000000000000 // indirect
+)
